0f65fafa245c22ffd7cd8454bbb7a550c9133131
LuisCastilloH <andres.castillo.hdez@gmail.com>
2021-03-13 15:46:10 -0600

    Exam 1 prep finished

--- exam1/exam1_prep/estaturas.c
+++ exam1/exam1_prep/estaturas.c
@@ -3,10 +3,13 @@
 
 typedef struct {
     int num;
-    char *name;
+    char name[20];
     int age;
-    double height;
-} member;
+    float height;
+} Member;
+
+void bubble_sort(Member mems[], int cont);
+void swap(Member *x, Member *y);
 
 int main() {
     FILE *inputFile, *outputFile;
@@ -18,6 +21,65 @@ int main() {
         exit(1);
     }
 
+    Member mems[100];
+    int cont = 0;
+    // lectura de archivo
+    while( fscanf(inputFile, "%d %s %d %f", &mems[cont].num, mems[cont].name,
+           &mems[cont].age, &mems[cont].height) != EOF ) {
+        cont += 1;
+    }
+    if( feof(inputFile) ) {
+        printf("File processed correctly\n");
+        printf("Numero de integrantes procesados %d\n", cont);
+    }
+    else {
+        printf("Error reading the file\n");
+        exit(1);
+    }
+    fclose(inputFile);
+
+    int ans;
+    printf("Cuantos integrantes quieres pasar al archivo?\n");
+    scanf("%d", &ans);
+    while( ans > cont ) {
+        printf("Elige maximo %d jugadores", cont);
+        printf("Cuantos integrantes quieres pasar al archivo?\n");
+        scanf("%d", &ans);
+    }
+    Member *memsFinal = malloc(sizeof(Member) * ans);
+    bubble_sort(mems, cont);
+    for(int i=0; i<ans; i++) {
+        memsFinal[i] = mems[i];
+    }
+
+    // escritura de archivo
+    outputFile = fopen("salidad.txt", "w");
+    fprintf(outputFile, "Num\tNom\t\tedad\test\n");
+    for(int i=0; i<ans; i++) {
+        fprintf(outputFile, "%d\t%s\t\t%d\t%.2f\n", memsFinal[i].num,
+                memsFinal[i].name, memsFinal[i].age, memsFinal[i].height);
+    }
+    fclose(outputFile);
+    free(memsFinal);
 
     return 0;
 }
+
+void swap(Member *x, Member *y) {
+    Member temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void
+bubble_sort(Member mems[], int cont) {
+    int i, j;
+    for (i=0; i<cont-1; i++) {
+        for (j=0; j<cont-i-1; j++) {
+            if (mems[j].height < mems[j+1].height) {
+                swap(&mems[j], &mems[j+1]);
+            }
+        }
+    }
+}
+
